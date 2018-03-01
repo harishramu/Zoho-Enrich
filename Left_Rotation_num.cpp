19523bb276939346c2c1832c0458619870ed8e05
Harish Ramu <harishramu097@gmail.com>
2018-03-01 18:47:50 +0530

    Add files via upload

---
+++ Left_Rotation_num.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int num,t,a[1000],n,i=0,temp,c=0;
+	cin>>num>>t;
+	while(num>0)
+	{
+	    a[i]=num%10;
+	    num=num/10;
+	    i++;
+	}
+	n=i;
+	while(c<t)
+	{
+	  temp=a[n-1];   
+	  for(i=n-2;i>=0;i--)
+	  {
+	    a[i+1]=a[i]; 
+      }
+	 a[0]=temp;
+	 c++;
+    }
+    
+    for(i=n-1;i>=0;i--)
+    {
+        cout<<a[i];
+    }
+    
+	return 0;
+}
